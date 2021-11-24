e0b69cbd67d156482dd5115b1f19a49a3a664409
Daniel Niedermühlbichler <daniel.niedermuehlbichler@gmail.com>
2021-11-24 18:58:52 +0100

    configure outputs for mysql endpoint

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,17 @@
+output "mysql_address" {
+  value = aws_db_instance.mysql-db.address
+}
+
+output "mysql_port" {
+  value = aws_db_instance.mysql-db.port
+}
+
 /*
 output "mysql_instance_id" {
   value = aws_db_instance.mysql-db.id
 }
 
-output "mysql_endpoint" {
-  value = aws_db_instance.mysql-db.endpoint
-}
+
 */
 
 #output "redis_instance_id" {
